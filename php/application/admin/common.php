e44e81d8e4e0d39c2094d2f76012190745b837ea
kaming <954087620@qq.com>
2020-12-19 14:46:02 +0800

    修复问题

--- php/application/admin/common.php
+++ php/application/admin/common.php
@@ -137,29 +137,29 @@ function profit($formid,$openid,$access_token)
 
 
 
-function postCurl($url,$data,$type)
-{
-    if($type == 'json'){
-        $data = json_encode($data);//对数组进行json编码
-        $header= array("Content-type: application/json;charset=UTF-8","Accept: application/json","Cache-Control: no-cache", "Pragma: no-cache");
-    }
-    $curl = curl_init();
-    curl_setopt($curl,CURLOPT_URL,$url);
-    curl_setopt($curl,CURLOPT_POST,1);
-    curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
-    curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
-    if(!empty($data)){
-        curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
-    }
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($curl,CURLOPT_HTTPHEADER,$header);
-    $res = curl_exec($curl);
-    if(curl_errno($curl)){
-        echo 'Error+'.curl_error($curl);
-    }
-    curl_close($curl);
-    return $res;
-}
+// function postCurl($url,$data,$type)
+// {
+//     if($type == 'json'){
+//         $data = json_encode($data);//对数组进行json编码
+//         $header= array("Content-type: application/json;charset=UTF-8","Accept: application/json","Cache-Control: no-cache", "Pragma: no-cache");
+//     }
+//     $curl = curl_init();
+//     curl_setopt($curl,CURLOPT_URL,$url);
+//     curl_setopt($curl,CURLOPT_POST,1);
+//     curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+//     curl_setopt($curl,CURLOPT_SSL_VERIFYHOST,false);
+//     if(!empty($data)){
+//         curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
+//     }
+//     curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+//     curl_setopt($curl,CURLOPT_HTTPHEADER,$header);
+//     $res = curl_exec($curl);
+//     if(curl_errno($curl)){
+//         echo 'Error+'.curl_error($curl);
+//     }
+//     curl_close($curl);
+//     return $res;
+// }
 
 
 
