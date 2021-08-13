eee48988fd169b6fddc6608281f6c2cd49857fd4
kamingluo <954087620@qq.com>
2021-08-13 17:37:19 +0800

    数据查看

--- php/application/football/controller/Statistics.php
+++ php/application/football/controller/Statistics.php
@@ -10,12 +10,54 @@ use think\Log;
 class Statistics
 {
    
-    //统计前一天的相关数据，一般是凌晨执行
-    public function payscore(Request $request)
+    //统计某个时间的数据
+    public function timeslot(Request $request)
     {
-
         set_time_limit(0);//设置超时时间
-        return  "统计失败";
+        $day=$request->param("day");
+        $time=date('Y-m-d',time());//获取当前时间
+        if($day==0){
+            //查询今天
+            $investment=db('record')->whereTime('create_time', 'today')->sum('money');
+            $transport=db('record')->where('state = 4  OR  state = 5')->whereTime('create_time', 'today')->sum('profit');
+            $win=db('record')->where('state = 1  OR  state = 2')->whereTime('create_time', 'today')->sum('profit');
+        }
+        else if($day==1){
+            //查询昨天
+            $investment=db('record')->whereTime('create_time', 'yesterday')->sum('money');
+            $transport=db('record')->where('state = 4  OR  state = 5')->whereTime('create_time', 'yesterday')->sum('profit');
+            $win=db('record')->where('state = 1  OR  state = 2')->whereTime('create_time', 'yesterday')->sum('profit');
+
+        }
+        else{
+            //查询这时间段
+            $newday=-$day.' days' ;
+            $newtime=date("Y-m-d", strtotime($newday));//计算的时间
+            $investment=db('record')-> where('create_time','>= time', $newtime)->sum('money');
+            $transport=db('record')->where('state = 4  OR  state = 5')-> where('create_time','>= time', $newtime)->sum('profit');
+            $win=db('record')->where('state = 1  OR  state = 2')-> where('create_time','>= time', $newtime)->sum('profit');
+        }
+
+        $data['investment']=$investment;
+        $data['transport']=$transport;
+        $data['win']=$win;
+        $state=['state'=> '200','message'  => "查询时间段数据",'data' => $data, ];
+        return  $state;
+    }
+
+
+    //统计全部
+    public function all(Request $request)
+    {
+        set_time_limit(0);//设置超时时间
+        $investment=db('record')->sum('money');//总投资
+        $transport=db('record')->where('state = 4  OR  state = 5')->sum('profit');//输
+        $win=db('record')->where('state = 1  OR  state = 2')->sum('profit');//赢
+        $data['investment']=$investment;
+        $data['transport']=$transport;
+        $data['win']=$win;
+        $state=['state'=> '200','message'  => "总数据",'data' => $data, ];
+        return  $state;
     }
    
 
